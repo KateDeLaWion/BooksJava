7fb4528046e29b14c34b2e69fcdb52906a503541
Student101 <student101@PSAdmins-Air-3.home>
2020-03-24 23:14:04 -0400

    first commit

---
+++ src/Books.java
@@ -0,0 +1,45 @@
+public class Books {
+    public static class Book{
+        String title;
+        String author;
+        int price
+        //constructor
+        public Author(String title, String author, int price){
+            this.title = title;
+            this.author = author;
+            this.price = price;
+        }
+        public void setTitle(String title) {
+            this.title = title;
+        }
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+        public void setPrice(int price) {
+            this.price = price;
+        }
+        public String getTitle() {
+            return title;
+        }
+        public String getAuthor() {
+            return author;
+        }
+        public int getPrice() {
+            return price;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
